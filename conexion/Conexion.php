e97997859a7f1c328cee53d8a7fd6360b5f4bcb2
jfleytas <jfleytas@users.noreply.github.com>
2022-02-16 16:01:35 -0300

    Initial code

---
+++ conexion/Conexion.php
@@ -0,0 +1,39 @@
+<?php
+class Conexion
+{
+    //private static $dbName = 'jesmari' ;
+    //private static $dbName = 'multiventas' ;
+    private static $dbName = 'ac-cell' ;
+    private static $dbHost = 'localhost' ;
+    private static $dbUsername = 'postgres';
+    private static $dbPassword = '123456789';
+     
+    private static $cont  = null;
+     
+    public function __construct() {
+        die('Init function is not allowed');
+    }
+     
+    public static function conexion()
+    {
+       // One connection through whole application
+       if ( null == self::$cont )
+       {     
+        try
+        {
+          self::$cont =  new PDO( "pgsql:host=".self::$dbHost.";"."dbname=".self::$dbName.";"."user=".self::$dbUsername.";"."password=".self::$dbPassword); 
+        }
+        catch(PDOException $e)
+        {
+          die($e->getMessage()); 
+        }
+       }
+       return self::$cont;
+    }
+     
+    public static function close()
+    {
+        self::$cont = null;
+    }
+}
+?>
